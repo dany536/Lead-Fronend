43f8084d16a006429fe8835b28119089efb6220e
dany536 <deepankar.advisior@gmail.com>
2024-09-08 15:19:56 +0530

    change in code

--- src/components/leadList.jsx
+++ src/components/leadList.jsx
@@ -23,8 +23,12 @@ const LeadList = () => {
   useEffect(() => {
     fetchLeads();
     const Token = Cookies.get('accessToken');
-    const decoded = jwtDecode(Token);
-    setUsers(decoded.user)
+    if (!Token) {
+      navigate('/')
+    } else {
+      const decoded = jwtDecode(Token);
+      setUsers(decoded.user)
+    }
   }, [Token]);
 
   const fetchLeads = async () => {
